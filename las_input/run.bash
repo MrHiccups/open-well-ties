b7919d1e830bbae832c13cef49e3b50750b439da
GeoMarky <mark.alvin.roberts@gmail.com>
2014-10-25 11:35:24 -0600

    Rename LAS file to remove spaces from file names.

--- las_input/run.bash
+++ las_input/run.bash
@@ -3,3 +3,7 @@
 # First copy Lasfiles from Google drive
 unzip ./Lasfiles.zip 
 
+#Rename to remove spaces for simplicity
+mv ./Lasfiles/Penobscot\ B-41\ LASOut_W4.las ./Lasfiles/Penobscot_B-41_LASOut_W4.las 
+mv ./Lasfiles/Penobscot\ L-30\ LASOut_W7.las ./Lasfiles/Penobscot_L-30_LASOut_W7.las 
+
