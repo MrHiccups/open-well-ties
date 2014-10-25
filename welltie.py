fb764ce2a56bad75147b6d3bd13e600f4942d1cf
Graham Dawes <gdawes@hotmail.com>
2014-10-25 15:42:30 -0600

    welltie.py now requests the graph

--- welltie.py
+++ welltie.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
+from evans_workflow import flow
+from StringIO import StringIO 
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+@app.route('/plot')
+def plot():
+    buffer = StringIO()
+    flow.plot('evans_workflow/L-30.las',buffer)
+    return buffer.getvalue()
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
