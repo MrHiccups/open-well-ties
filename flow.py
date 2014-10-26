eca9e84858b604a35f71176e47c9f65ff8ed626e
GeoMarky <mark.alvin.roberts@gmail.com>
2014-10-26 15:00:47 -0600

    Updated flow with amplitudes balanced.

--- flow.py
+++ flow.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import matplotlib as mpl
 mpl.use('pdf')
+from scipy import signal
 import matplotlib.pyplot as plt
 
 sys.path.append('./synth')
@@ -19,7 +20,7 @@ from synth.synth import *
 start_z = 2300
 end_z = 2600
 
-start_t = 1.4
+start_t = 1.
 end_t = 2.5
 
 # In [15] (shoudl go into a function)
@@ -55,8 +56,11 @@ plot_spectrum('./outputs/trace_spectrum.pdf', 'pdf', real_trace)
 
 # Generate gabor filter and apply to logs
 gab = gabor_filt()
-synth2 = np.convolve(gab, RC_t, mode='same') 
-synth2 = np.resize(synth2, t.size)
+#synth2 = np.convolve(gab, RC_t, mode='same') 
+#synth2 = np.resize(synth2, t.size)
+synth2 = synthetic 
+
+synth2 = synth2 * 30000
 
 plot_spectrum('./outputs/well_synth2_spectrum.pdf', 'pdf', synth2)
 
@@ -64,6 +68,28 @@ plot_logs('./outputs/real_trace2.pdf', 'pdf', t, real_trace, synth2, start_t, en
 
 corr = correlation(real_trace, synth2, 251, 703)
 
+print corr
+
+# Now calculate Wiener filter
+wf = wiener_filter(real_trace, synth2, 251, 703, SNR=10)
+#wf = wiener_filter(synth2, real_trace, 251, 703)
+twf = np.arange(0, 452*0.004, 0.004)
+
+
+plot_logs('./outputs/wiener_filter.pdf', 'pdf', twf, wf, wf, 0, 1.8)
+
+print real_trace.size
+#new_trace, remainder = signal.deconvolve( real_trace, wf)
+new_trace = np.convolve(wf, real_trace, mode='same')
+new_trace = np.resize(new_trace, t.size)
+
+print "new trace size"
+print new_trace.size
+print synth2.size
+print t.size
+
+plot_logs('./outputs/real_trace3.pdf', 'pdf', t, new_trace, synth2, start_t, end_t, title='Real Trace')
+corr = correlation(new_trace, synth2, 251, 703)
 print corr
 
 
