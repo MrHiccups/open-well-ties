8ecc9c870fdc156d4fbe24717ab77990a50da010
Graham Dawes <gdawes@hotmail.com>
2014-10-26 10:06:24 -0600

    Correct content type (png image)  is  now reported for the url /plot

--- welltie.py
+++ welltie.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 from synth.synth import generate_reflectivity, plot_logs
 from StringIO import StringIO 
 
@@ -10,15 +10,18 @@ def index():
 
 @app.route('/plot')
 def plot():
-    buffer = StringIO()
     t, RC_t = generate_reflectivity()
     start_z = 2300
     end_z = 2600
 
     start_t = 1.4
     end_t = 2.5
-    plot_logs(buffer, 'png', t, RC_t, RC_t, start_t, end_t, title='synthetic')
-    return buffer.getvalue()
+
+    buffer = StringIO()
+    plot_logs(buffer, 'png', t, RC_t, RC_t, start_t, end_t, title='reflectiviy') 
+    response = make_response(buffer.getvalue())
+    response.headers['Content-Type'] ='image/png'
+    return response
     
 
 if __name__ == '__main__':
